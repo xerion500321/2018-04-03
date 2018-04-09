0232fd3b112424d6d219a2610dc9ff8e12d1dd00
xerion500321 <wl03420366>
2018-04-09 22:10:26 +0800

    2018-04-03

--- Main.java
+++ Main.java
@@ -11,9 +11,9 @@ public class Main {
         boolean x = true;
         while (x) {
             System.out.println("輸入資料請按1");
-            System.out.println("修改資料請按2");
-            System.out.println("資料請按3");
-            System.out.println("顯示資料請按4");
+            System.out.println("顯示資料請按2");
+            System.out.println("刪除資料請按3");
+            System.out.println("修改資料請按4");
             System.out.println("結束請按5");
             int s = scn.nextInt();
             int steve = 0;
@@ -35,6 +35,25 @@ public class Main {
                         }
                         break;
                     case 2:
+                        for (int i = 0; i < per.size(); i++) {
+                            Person lol = per.get(i);
+                            lol.showAll();
+                        }
+                        break;
+                case 3:
+                    System.out.println("輸入欲刪除之角色");
+                    name = scn.next();
+                    for (int i = 0; i < per.size(); i++) {
+                        Person lol = per.get(i);
+                        if (lol.getName().equals(name)) {
+                            lol.showAll();
+                            steve = i;
+                            i = per.size(); //停止迴圈
+                        }
+                    }
+                    per.remove(steve);
+                    System.out.println("資料已刪除");
+                case 4:
                     System.out.println("請輸入欲修改之角色ID");
                     name = scn.next();
                     for (int i = 0; i < per.size(); i++) {
@@ -56,25 +75,6 @@ public class Main {
                     System.out.println("輸入回復力");
                     res = scn.nextFloat();
                     per.set(steve,new Person(name, category, attack, blood, res));
-                    break;
-                case 3:
-                    System.out.println("輸入欲刪除之角色");
-                    name = scn.next();
-                    for (int i = 0; i < per.size(); i++) {
-                        Person lol = per.get(i);
-                        if (lol.getName().equals(name)) {
-                            lol.showAll();
-                            steve = i;
-                            i = per.size(); //停止迴圈
-                        }
-                    }
-                    per.remove(steve);
-                    System.out.println("資料已刪除");
-                case 4:
-                    for (int i = 0; i < per.size(); i++) {
-                        Person lol = per.get(i);
-                        lol.showAll();
-                    }
                     break;
                 case 5:
                     x = false;
